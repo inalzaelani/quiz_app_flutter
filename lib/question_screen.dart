7ad672baeaed419e14f8b119b79c7df831f2d31f
Muhammad Inal Zaelani <inalzaelani2806@gmail.com>
2023-07-15 20:04:22 +0700

    [ADD] Stateful Widget

---
+++ lib/question_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class QuestionScreen extends StatefulWidget{
+  const QuestionScreen({super.key});
+
+  @override
+  State<QuestionScreen> createState() {
+    // TODO: implement createState
+    return _QuestionScreenState();
+  }
+}
+
+class _QuestionScreenState extends State<QuestionScreen>{
+  @override
+  Widget build(context) {
+    // TODO: implement build
+    return const Text(
+      "Selamat Datang Memet"
+    );
+  }
+
+}
