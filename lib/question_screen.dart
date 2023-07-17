5dc620abe7dcb318c1c5888f4d3bc27d539b7b13
Muhammad Inal Zaelani <inalzaelani2806@gmail.com>
2023-07-17 08:20:44 +0700

    [FIX] Configure Column Quiz

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class QuestionScreen extends StatefulWidget{
+class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
 
   @override
@@ -10,13 +10,23 @@ class QuestionScreen extends StatefulWidget{
   }
 }
 
-class _QuestionScreenState extends State<QuestionScreen>{
+class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(context) {
     // TODO: implement build
-    return const Text(
-      "Selamat Datang Memet"
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The Question...'),
+          const SizedBox(height: 30),
+          ElevatedButton(onPressed: () {}, child: const Text('Answer 1')),
+          ElevatedButton(onPressed: () {}, child: const Text('Answer 2')),
+          ElevatedButton(onPressed: () {}, child: const Text('Answer 3')),
+          ElevatedButton(onPressed: () {}, child: const Text('Answer 4')),
+        ],
+      ),
     );
   }
-
 }
